41b8e54fa21dd75d49b39e3104559d4c0278f690
almondjain <49396182+almondjain@users.noreply.github.com>
2021-09-28 00:49:45 +0530

    started js

---
+++ script.js
@@ -0,0 +1,9 @@
+var bdayDate = document.querySelector("#bday-input");
+var checkbtn = document.querySelector("#check-btn");
+var result = document.querySelector(".output");
+
+function checkBday(){
+
+}
+
+checkbtn.addEventListener("click",checkBday);
